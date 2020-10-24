63299c2e705694f5427bd22320deb065379cd949
VoduDay <andriy.maniak@gmail.com>
2020-10-24 17:50:10 +0300

    Updates

---
+++ precache-manifest.5f2d369b907450bacec5b293b3c0616a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d645e7fd037a490960cddfb6d3d865f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "03d495ad825b05bd142a",
+    "url": "./static/css/main.c1981675.chunk.css"
+  },
+  {
+    "revision": "e1dbccfc8cc249e289a2",
+    "url": "./static/js/2.4c149efb.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.4c149efb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d495ad825b05bd142a",
+    "url": "./static/js/main.9df247d9.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
